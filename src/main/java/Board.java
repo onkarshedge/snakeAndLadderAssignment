417b88fe5e8d9d9d2c428058476f23382141a246
Onkar Shedge <oshedge@outlook.com>
2020-05-26 10:31:21 +0530

    Board should return the current position if next position is greater than 100.

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,5 +1,11 @@
 public class Board {
+    public static final int BOARD_LIMIT = 100;
+
     public int nextPosition(int currentPosition, int diceOutcome) {
-        return currentPosition + diceOutcome;
+        int nextPosition = currentPosition + diceOutcome;
+        if (nextPosition > BOARD_LIMIT) {
+            return currentPosition;
+        }
+        return nextPosition;
     }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -14,4 +14,16 @@ public class BoardTest {
 
         assertEquals(9, actualPosition);
     }
+
+    @Test
+    public void shouldReturnNextPositionAsSameWhenNextPositionIsOutsideBoard() {
+        Board board = new Board();
+        int currentPosition = 96;
+        int diceOutcome = 5;
+
+        int actualPosition = board.nextPosition(currentPosition, diceOutcome);
+
+        assertEquals(96, actualPosition);
+    }
+
 }
